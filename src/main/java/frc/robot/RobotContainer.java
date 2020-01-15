a068cff0a51305f2d02aa3f08e3e936a8b23d5ef
Pablo Casals <p-casals@users.noreply.github.com>
2020-01-15 17:33:30 +0000

    cleaned up code; moved shootercommand methods to robotcontainer

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -10,7 +10,10 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.FunctionalCommand;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.StartEndCommand;
+import edu.wpi.first.wpilibj2.command.Subsystem;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import frc.robot.commands.auto.routines.TestAutoCommandGroup;
 import frc.robot.subsystems.*;
@@ -20,8 +23,11 @@ public class RobotContainer {
     // Subsystems
     private final Drivetrain DRIVETRAIN = new Drivetrain();
     private final Climber CLIMBER = new Climber();
+    private final ShooterSubsystem SHOOTER = new ShooterSubsystem();
 
     // Commands
+
+    //CLIMBER COMMANDS
     private final StartEndCommand climb = new StartEndCommand(
             () -> CLIMBER.reverseHookPiston(),
             () -> CLIMBER.stopHookPiston(),
@@ -44,6 +50,15 @@ public class RobotContainer {
             () -> CLIMBER.reverseRaisePiston(),
             () -> CLIMBER.stopRaisePiston(),
             CLIMBER
+    );
+
+    //SHOOTER COMMANDS
+    
+    private final StartEndCommand shootAtSpeed = new StartEndCommand(
+        // TODO: change setspeed parameter to variable if vision processing works.
+        () -> SHOOTER.setSpeed(1),
+        () -> SHOOTER.makeZero(),
+        SHOOTER
     );
 
     // Controllers
