d3d79ca4a0e572626ae25a00865fb7fbd23da3a0
LordSquishers <lordsquisherton@gmail.com>
2020-01-18 10:35:42 +0000

    created drivetrain branch

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -24,28 +24,10 @@ public final class Constants {
   public static final int BACK_LEFT_DRIVE_MOTOR = 1;
   public static final int FRONT_RIGHT_DRIVE_MOTOR = 2;
   public static final int BACK_RIGHT_DRIVE_MOTOR = 3;
-  
-  // INTAKE
-  public static final int WHEEL_INTAKE_MOTOR = 4;
-  public static final int DEPLOY_INTAKE_MOTOR = 5;
-  
-  // PISTONS
-  public static final int CLIMBER_PISTON_LIFT_PISTON_1 = 0;
-  public static final int CLIMBER_PISTON_LIFT_PISTON_2 = 1;
-  public static final int CLIMBER_ROBOT_LIFT_PISTON_1 = 2;
-  public static final int CLIMBER_ROBOT_LIFT_PISTON_2 = 3;
-
-  public static final int INTAKE_PISTON_1 = 4;
-  public static final int INTAKE_PISTON_2 = 5;
 
   // ENCODERS
   public static final int LEFT_ENCODER_PORT = 0; // port 1 is reserved
   public static final int RIGHT_ENCODER_PORT = 2; // port 3 is reserved
-  
-  // SHOOTERS
-  public static final int SHOOTER_MOTOR_1 = 7;
-  public static final int SHOOTER_MOTOR_2 = 8;
-  public static final int SHOOTER_MOTOR_3 = 9;
 
   // === CONTROLLERS === //
   
@@ -55,39 +37,9 @@ public final class Constants {
   public static final int FORWARD_AXIS_RIGHT = 2;
   public static final int HORIZ_AXIS_RIGHT = 3;
 
-  // TRIGGERS
-  public static final int LEFT_TRIGGER_AXIS = 7;
-
   // CONTROLLERS
   public static final int DRIVER_CONTROLLER = 0;
   public static final int OPERATOR_CONTROLLER = 1;
-
-  // CLIMBING
-  public static final int CLIMB_BUTTON = 1;
-  public static final int RAISE_HOOKS_BUTTON = 2;
-  public static final int RAISE_CLIMB_PISTONS_BUTTON  = 3;
-  public static final int LOWER_CLIMB_PISTONS_BUTTON = 4;
-
-  // SHOOTING
-  
-  // TODO: Change this number
-  public static final int SHOOT_BUTTON = 7;
-
-  // INTAKE
-
-  // TODO: MAKE TOGGLEABLE???
-  public static final int DEPLOY_INTAKE = 5;
-  public static final int RETRACT_INTAKE = 6;
-  public static final int END_INTAKE = 8;
-  public static final int RUN_INTAKE = 9;
-
-
-  
-  // === SPEED CONSTANTS === //
-  
-  // INTAKE
-  public static final double DEPLOY_INTAKE_SPEED = 0.5;
-  public static final double WHEEL_INTAKE_SPEED = 0.5;
   
 }
 
