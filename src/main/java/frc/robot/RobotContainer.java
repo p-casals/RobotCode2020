ff1226a88e3c4685e1e69fcf4033060fbb9d7c23
Pablo Casals <p-casals@users.noreply.github.com>
2020-01-25 10:35:56 +0000

    it was messed up and now it might be fixed

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -69,15 +69,16 @@ public class RobotContainer {
 
         // CONTROL PANEL BUTTONS
         controlSpinButton.whenHeld(robotCommands.controlSpin);
+        
         int x = 0;
         if (x == 0) {
-            controlLiftButton.cancelWhenPressed(robotCommands.controlDrop);
-            controlLiftButton.whenPressed(robotCommands.controlLift.withTimeout(3));
+            controlLiftButton.toggleWhenPressed(robotCommands.controlDrop);
+            //controlLiftButton.whenPressed(robotCommands.controlLift.withTimeout(3));
             x = 1;
         }
         else {
-            controlLiftButton.cancelWhenPressed(robotCommands.controlLift);
-            controlLiftButton.whenPressed(robotCommands.controlDrop.withTimeout(3));
+            controlLiftButton.toggleWhenPressed(robotCommands.controlLift);
+            //controlLiftButton.whenPressed(robotCommands.controlDrop.withTimeout(3));
             x = 0;
         }
     }
